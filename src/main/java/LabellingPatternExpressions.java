f3525b8a427978723de507f3fa58ee68a7df1b21
Szymon Lepianka <slepianka@wp.pl>
2022-11-13 17:16:20 +0100

    adds algorithm 1

--- src/main/java/LabellingPatternExpressions.java
+++ src/main/java/LabellingPatternExpressions.java
@@ -1,21 +1,29 @@
+import java.util.*;
+
 public class LabellingPatternExpressions {
 
+    // Algorithm 1 - Labelling pattern expressions
+    //
     // Input: pattern expression w, ex. Seq(a, Seq(Concur(b, c, d), Concur Re(e, f, g)))
+    // Output: function result, that is 'Labelling' as labelled pattern expression w',
+    //                          ex. Seq(1]a, Seq(2]Concur(3]b, c,d[3), Concur Re(3]e, f , g[3)[2)[1)
+    //
+    // Lexical tokens are processed/scanned one by one from left to right
     public static String labelExpressions(String expression) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder labelledExpression = new StringBuilder();
         int labelNumber = 0;
         for (char c : expression.toCharArray()) {
             if (c == '(') {
                 labelNumber++;
-                result.append("(" + labelNumber + "]");
+                labelledExpression.append("(").append(labelNumber).append("]");
             } else if (c == ')') {
-                result.append("[" + labelNumber + ")");
+                labelledExpression.append("[").append(labelNumber).append(")");
                 labelNumber--;
             } else {
-                result.append(c);
+                labelledExpression.append(c);
             }
         }
-        return result.toString();
+        return labelledExpression.toString();
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+
+        // Loading of workflow patterns from batch files
         String patternRulesFolFile = "./pattern_rules/pattern_rules_FOL.json";
         String patternRulesLtlFile = "./pattern_rules/pattern_rules_LTL.json";
         List<WorkflowPattern> folWorkflowPatterns = WorkflowPattern.loadWorkflowPatterns(patternRulesFolFile);
         List<WorkflowPattern> ltlWorkflowPatterns = WorkflowPattern.loadWorkflowPatterns(patternRulesLtlFile);
+
+        // Algorithm 1 - Labelling pattern expressions
+        String exampleExpression1 = "Seq(a, Seq(Concur(b,c,d), ConcurRe(e,f,g)))";
+        String exampleExpression2 = "Seq(a100, Seq(Concur(b101, c102, d103), ConcurRe(e104, f105, g106)))";
+        String labelledPatternExpression1 = LabellingPatternExpressions.labelExpressions(exampleExpression1);
+        String labelledPatternExpression2 = LabellingPatternExpressions.labelExpressions(exampleExpression2);
+        System.out.println(labelledPatternExpression1);
+        System.out.println(labelledPatternExpression2);
     }
 }
